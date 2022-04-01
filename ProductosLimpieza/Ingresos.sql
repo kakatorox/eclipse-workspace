b3e27e45c6e46c625c55ec6f84f9c69aa6d532f7
kakatorox <felipetorresmora@outlook.es>
2022-04-01 17:45:21 -0300

    scritp de ingreso de datos

---
+++ ProductosLimpieza/Ingresos.sql
@@ -0,0 +1,11 @@
+INSERT INTO categoria(nombre_categoria)
+VALUES ('Detergente líquido');
+
+INSERT INTO categoria(nombre_categoria)
+VALUES ('Detergente en polvo');
+
+INSERT INTO producto(nombre_producto, precio_producto, descripcion_producto, id_categoria)
+VALUES ('Omo', 3500, 'Quita manchas', 1);
+
+INSERT INTO producto(nombre_producto, precio_producto, descripcion_producto, id_categoria)
+VALUES ('Ariel', 4400, 'Quita manchas', 2);
